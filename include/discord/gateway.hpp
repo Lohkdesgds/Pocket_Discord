b61583368f8a7a8c1065ea44e48ff3ace5e19b08
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2023-12-26 12:04:25 -0300

    Middle of things, backup

--- include/discord/gateway.hpp
+++ include/discord/gateway.hpp
@@ -9,6 +9,7 @@
 
 #include "../eventhandler.h"
 
+
 namespace Lunaris {
     namespace PocketDiscord {
 
@@ -21,5 +22,133 @@ namespace Lunaris {
         constexpr unsigned gateway_queue_size_default = 30;
         constexpr unsigned gateway_poll_event_priority = 50;
         
+        const char app_version[] = "V2.0.0 ALPHA";
+        const char target_app[] = "ESP32";
+        
+        // from Discord @ https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-opcodes
+        enum class gateway_opcodes : int16_t {
+            UNKNOWN                 = -1,       // [MINE]           // Default creation, not related to Discord.
+            DISPATCH                = 0,        // [RECEIVE]        // An event was dispatched.
+            HEARTBEAT               = 1,        // [SEND/RECEIVE]   // Fired periodically by the client to keep the connection alive.
+            IDENTIFY                = 2,        // [SEND]           // Starts a new session during the initial handshake.
+            PRESENCE_UPDATE_SEND    = 3,        // [SEND]           // Update the client's presence.
+            VOICE_STATE_UPDATE      = 4,        // [SEND]           // Used to join/leave or move between voice channels.
+            RESUME                  = 6,        // [SEND]           // Resume a previous session that was disconnected.
+            RECONNECT               = 7,        // [RECEIVE]        // You should attempt to reconnect and resume immediately.
+            REQUEST_GUILD_MEMBERS   = 8,        // [SEND]           // Request information about offline guild members in a large guild.
+            INVALID_SESSION         = 9,        // [RECEIVE]        // The session has been invalidated. You should reconnect and identify/resume accordingly.
+            HELLO                   = 10,       // [RECEIVE]        // Sent immediately after connecting, contains the heartbeat_interval to use.
+            HEARTBEAT_ACK           = 11,       // [RECEIVE]        // Sent in response to receiving a heartbeat to acknowledge that it has been received.
+            _MAX = HEARTBEAT_ACK                // for reference
+        };
+        static gateway_opcodes str2gateway_opcode(const char*);
+
+        // from Discord @ https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-close-event-codes
+        enum class gateway_close_event_codes : int16_t {
+            UNKNOWN_ERROR               = 4000,	    // We're not sure what went wrong. Try reconnecting?
+            UNKNOWN_OPCODE              = 4001,	    // You sent an invalid Gateway opcode or an invalid payload for an opcode. Don't do that!
+            DECODE_ERROR                = 4002,	    // You sent an invalid payload to us. Don't do that!
+            NOT_AUTHENTICATED           = 4003,	    // You sent us a payload prior to identifying.
+            AUTHENTICATION_FAILED       = 4004,	    // The account token sent with your identify payload is incorrect.
+            ALREADY_AUTHENTICATED       = 4005,	    // You sent more than one identify payload. Don't do that!
+            INVALID_SEQ                 = 4007,	    // The sequence sent when resuming the session was invalid. Reconnect and start a new session.
+            RATE_LIMITED                = 4008,	    // Woah nelly! You're sending payloads to us too quickly. Slow it down! You will be disconnected on receiving this.
+            SESSION_TIMED_OUT           = 4009,	    // Your session timed out. Reconnect and start a new one.
+            INVALID_SHARD               = 4010,	    // You sent us an invalid shard when identifying.
+            SHARDING_REQUIRED           = 4011,	    // The session would have handled too many guilds - you are required to shard your connection in order to connect.
+            INVALID_API_VERSION         = 4012,	    // You sent an invalid version for the gateway.
+            INVALID_INTENTS             = 4013,	    // You sent an invalid intent for a Gateway Intent. You may have incorrectly calculated the bitwise value.
+            DISALLOWED_INTENTS          = 4014	    // You sent a disallowed intent for a Gateway Intent. You may have tried to specify an intent that you have not enabled or are not approved for.
+        };
+        static gateway_close_event_codes str2gateway_close_event_codes(const char*);
+
+        // from Discord @ https://discord.com/developers/docs/topics/gateway-events#send-events
+        // previously called gateway_commands
+        enum class gateway_send_events : int16_t {
+            UNKNOWN                 = static_cast<int>(gateway_opcodes::UNKNOWN),                   // Mine, unknown.
+            IDENTIFY                = static_cast<int>(gateway_opcodes::IDENTIFY),                  // Triggers the initial handshake with the gateway
+            RESUME                  = static_cast<int>(gateway_opcodes::RESUME),                    // Resumes a dropped gateway connection
+            HEARTBEAT               = static_cast<int>(gateway_opcodes::HEARTBEAT),                 // Maintains an active gateway connection
+            REQUEST_GUILD_MEMBERS   = static_cast<int>(gateway_opcodes::REQUEST_GUILD_MEMBERS),     // Requests members for a guild
+            UPDATE_VOICE_STATE      = static_cast<int>(gateway_opcodes::VOICE_STATE_UPDATE),        // Joins, moves, or disconnects the client from a voice channel
+            UPDATE_PRESENCE         = static_cast<int>(gateway_opcodes::PRESENCE_UPDATE_SEND)       // Updates a client's presence
+        };
+        static gateway_send_events str2gateway_send_events(const char*);
+
+        
+        // from Discord @ https://discord.com/developers/docs/topics/gateway#commands-and-events-gateway-events
+        // this happens as STRING_THING
+        enum class gateway_events : int16_t {
+            UNKNOWN = -1,                               // Extra case we don't know
+            HELLO,                                      // Defines the heartbeat interval
+            READY,                                      // Contains the initial state information
+            RESUMED,                                    // Response to Resume
+            RECONNECT,                                  // Server is going away, client should reconnect to gateway and resume
+            INVALID_SESSION,                            // Failure response to Identify or Resume or invalid active session
+            APPLICATION_COMMAND_PERMISSION_UPDATE,      // Application command permission was updated
+            AUTO_MODERATION_RULE_CREATE,                // Auto Moderation rule was created
+            AUTO_MODERATION_RULE_UPDATE,                // Auto Moderation rule was updated
+            AUTO_MODERATION_RULE_DELETE,                // Auto Moderation rule was deleted
+            AUTO_MODERATION_ACTION_EXECUTION,           // Auto Moderation rule was triggered and an action was executed (e.g. a message was blocked)
+            CHANNEL_CREATE,                             // New guild channel created
+            CHANNEL_UPDATE,                             // Channel was updated
+            CHANNEL_DELETE,                             // Channel was deleted
+            CHANNEL_PINS_UPDATE,                        // Message was pinned or unpinned
+            THREAD_CREATE,                              // Thread created, also sent when being added to a private thread
+            THREAD_UPDATE,                              // Thread was updated
+            THREAD_DELETE,                              // Thread was deleted
+            THREAD_LIST_SYNC,                           // Sent when gaining access to a channel, contains all active threads in that channel
+            THREAD_MEMBER_UPDATE,                       // Thread member for the current user was updated
+            THREAD_MEMBERS_UPDATE,                      // Some user(s) were added to or removed from a thread
+            ENTITLEMENT_CREATE,                         // Entitlement was created
+            ENTITLEMENT_UPDATE,                         // Entitlement was updated or renewed
+            ENTITLEMENT_DELETE,                         // Entitlement was deleted
+            GUILD_CREATE,                               // Lazy-load for unavailable guild, guild became available, or user joined a new guild
+            GUILD_UPDATE,                               // Guild was updated
+            GUILD_DELETE,                               // Guild became unavailable, or user left/was removed from a guild
+            GUILD_AUDIT_LOG_ENTRY_CREATE,               // A guild audit log entry was created
+            GUILD_BAN_ADD,                              // User was banned from a guild
+            GUILD_BAN_REMOVE,                           // User was unbanned from a guild
+            GUILD_EMOJIS_UPDATE,                        // Guild emojis were updated
+            GUILD_STICKERS_UPDATE,                      // Guild stickers were updated
+            GUILD_INTEGRATIONS_UPDATE,                  // Guild integration was updated
+            GUILD_MEMBER_ADD,                           // New user joined a guild
+            GUILD_MEMBER_REMOVE,                        // User was removed from a guild
+            GUILD_MEMBER_UPDATE,                        // Guild member was updated
+            GUILD_MEMBERS_CHUNK,                        // Response to Request Guild Members
+            GUILD_ROLE_CREATE,                          // Guild role was created
+            GUILD_ROLE_UPDATE,                          // Guild role was updated
+            GUILD_ROLE_DELETE,                          // Guild role was deleted
+            GUILD_SCHEDULED_EVENT_CREATE,               // Guild scheduled event was created
+            GUILD_SCHEDULED_EVENT_UPDATE,               // Guild scheduled event was updated
+            GUILD_SCHEDULED_EVENT_DELETE,               // Guild scheduled event was deleted
+            GUILD_SCHEDULED_EVENT_USER_ADD,             // User subscribed to a guild scheduled event
+            GUILD_SCHEDULED_EVENT_USER_REMOVE,          // User unsubscribed from a guild scheduled event
+            INTEGRATION_CREATE,                         // Guild integration was created
+            INTEGRATION_UPDATE,                         // Guild integration was updated
+            INTEGRATION_DELETE,                         // Guild integration was deleted
+            INTERACTION_CREATE,                         // User used an interaction, such as an Application Command
+            INVITE_CREATE,                              // Invite to a channel was created
+            INVITE_DELETE,                              // Invite to a channel was deleted
+            MESSAGE_CREATE,                             // Message was created
+            MESSAGE_UPDATE,                             // Message was edited
+            MESSAGE_DELETE,                             // Message was deleted
+            MESSAGE_DELETE_BULK,                        // Multiple messages were deleted at once
+            MESSAGE_REACTION_ADD,                       // User reacted to a message
+            MESSAGE_REACTION_REMOVE,                    // User removed a reaction from a message
+            MESSAGE_REACTION_REMOVE_ALL,                // All reactions were explicitly removed from a message
+            MESSAGE_REACTION_REMOVE_EMOJI,              // All reactions for a given emoji were explicitly removed from a message
+            PRESENCE_UPDATE,                            // User was updated
+            STAGE_INSTANCE_CREATE,                      // Stage instance was created
+            STAGE_INSTANCE_UPDATE,                      // Stage instance was updated
+            STAGE_INSTANCE_DELETE,                      // Stage instance was deleted or closed
+            TYPING_START,                               // User started typing in a channel
+            USER_UPDATE,                                // Properties about the user changed
+            VOICE_STATE_UPDATE,                         // Someone joined, left, or moved a voice channel
+            VOICE_SERVER_UPDATE,                        // Guild's voice server was updated
+            WEBHOOKS_UPDATE                             // Guild channel webhook was created, update, or deleted
+        };
+        static gateway_events str2gateway_events(const char*);
+
     }
 }
--- src/discord/gateway.cpp
+++ src/discord/gateway.cpp
@@ -1,1 +1,149 @@
 #include "discord/gateway.hpp"
+#include <unordered_map>
+
+namespace Lunaris {
+    namespace PocketDiscord {
+        
+        gateway_opcodes str2gateway_opcode(const char* s)
+        {
+            // size: 80 bytes + dynamic alloc
+            static std::unordered_map<const char*, gateway_opcodes> const map = {
+                { "DISPATCH"                , gateway_opcodes::DISPATCH },
+                { "HEARTBEAT"               , gateway_opcodes::HEARTBEAT },
+                { "IDENTIFY"                , gateway_opcodes::IDENTIFY },
+                { "PRESENCE_UPDATE_SEND"    , gateway_opcodes::PRESENCE_UPDATE_SEND },
+                { "VOICE_STATE_UPDATE"      , gateway_opcodes::VOICE_STATE_UPDATE },
+                { "RESUME"                  , gateway_opcodes::RESUME },
+                { "RECONNECT"               , gateway_opcodes::RECONNECT },
+                { "REQUEST_GUILD_MEMBERS"   , gateway_opcodes::REQUEST_GUILD_MEMBERS },
+                { "INVALID_SESSION"         , gateway_opcodes::INVALID_SESSION },
+                { "HELLO"                   , gateway_opcodes::HELLO },
+                { "HEARTBEAT_ACK"           , gateway_opcodes::HEARTBEAT_ACK }
+            };
+
+            if (const auto& it = map.find(s); it != map.end()) return it->second;
+            return gateway_opcodes::UNKNOWN;
+        }
+
+        gateway_close_event_codes str2gateway_opcode(const char* s)
+        {
+            // size: 80 bytes + dynamic alloc
+            static std::unordered_map<const char*, gateway_close_event_codes> const gateway_opcodes_map = {
+                { "UNKNOWN_ERROR"             , gateway_close_event_codes::UNKNOWN_ERROR },
+                { "UNKNOWN_OPCODE"            , gateway_close_event_codes::UNKNOWN_OPCODE },
+                { "DECODE_ERROR"              , gateway_close_event_codes::DECODE_ERROR },
+                { "NOT_AUTHENTICATED"         , gateway_close_event_codes::NOT_AUTHENTICATED },
+                { "AUTHENTICATION_FAILED"     , gateway_close_event_codes::AUTHENTICATION_FAILED },
+                { "ALREADY_AUTHENTICATED"     , gateway_close_event_codes::ALREADY_AUTHENTICATED },
+                { "INVALID_SEQ"               , gateway_close_event_codes::INVALID_SEQ },
+                { "RATE_LIMITED"              , gateway_close_event_codes::RATE_LIMITED },
+                { "SESSION_TIMED_OUT"         , gateway_close_event_codes::SESSION_TIMED_OUT },
+                { "INVALID_SHARD"             , gateway_close_event_codes::INVALID_SHARD },
+                { "SHARDING_REQUIRED"         , gateway_close_event_codes::SHARDING_REQUIRED },
+                { "INVALID_API_VERSION"       , gateway_close_event_codes::INVALID_API_VERSION },
+                { "INVALID_INTENTS"           , gateway_close_event_codes::INVALID_INTENTS },
+                { "DISALLOWED_INTENTS"        , gateway_close_event_codes::DISALLOWED_INTENTS }
+            };
+
+            if (const auto& it = map.find(s); it != map.end()) return it->second;
+            return gateway_opcodes::UNKNOWN_ERROR;
+        }
+
+
+        gateway_send_events str2gateway_send_events(const char* s)
+        {
+            // size: 80 bytes + dynamic alloc
+            static std::unordered_map<const char*, gateway_send_events> const gateway_opcodes_map = {
+                { "UNKNOWN"                    , gateway_send_events::UNKNOWN },
+                { "IDENTIFY"                   , gateway_send_events::IDENTIFY },
+                { "RESUME"                     , gateway_send_events::RESUME },
+                { "HEARTBEAT"                  , gateway_send_events::HEARTBEAT },
+                { "REQUEST_GUILD_MEMBERS"      , gateway_send_events::REQUEST_GUILD_MEMBERS },
+                { "UPDATE_VOICE_STATE"         , gateway_send_events::UPDATE_VOICE_STATE },
+                { "UPDATE_PRESENCE"            , gateway_send_events::UPDATE_PRESENCE }
+            };
+
+            if (const auto& it = map.find(s); it != map.end()) return it->second;
+            return gateway_send_events::UNKNOWN;
+        }
+        
+        gateway_events str2gateway_events(const char* s)
+        {
+            // size: 80 bytes + dynamic alloc
+            static std::unordered_map<const char*, gateway_events> const gateway_opcodes_map = {
+                { "UNKNOWN"                                 , gateway_events::UNKNOWN},
+                { "HELLO"                                   , gateway_events::HELLO},
+                { "READY"                                   , gateway_events::READY},
+                { "RESUMED"                                 , gateway_events::RESUMED},
+                { "RECONNECT"                               , gateway_events::RECONNECT},
+                { "INVALID_SESSION"                         , gateway_events::INVALID_SESSION},
+                { "APPLICATION_COMMAND_PERMISSION_UPDATE"   , gateway_events::APPLICATION_COMMAND_PERMISSION_UPDATE},
+                { "AUTO_MODERATION_RULE_CREATE"             , gateway_events::AUTO_MODERATION_RULE_CREATE},
+                { "AUTO_MODERATION_RULE_UPDATE"             , gateway_events::AUTO_MODERATION_RULE_UPDATE},
+                { "AUTO_MODERATION_RULE_DELETE"             , gateway_events::AUTO_MODERATION_RULE_DELETE},
+                { "AUTO_MODERATION_ACTION_EXECUTION"        , gateway_events::AUTO_MODERATION_ACTION_EXECUTION},
+                { "CHANNEL_CREATE"                          , gateway_events::CHANNEL_CREATE},
+                { "CHANNEL_UPDATE"                          , gateway_events::CHANNEL_UPDATE},
+                { "CHANNEL_DELETE"                          , gateway_events::CHANNEL_DELETE},
+                { "CHANNEL_PINS_UPDATE"                     , gateway_events::CHANNEL_PINS_UPDATE},
+                { "THREAD_CREATE"                           , gateway_events::THREAD_CREATE},
+                { "THREAD_UPDATE"                           , gateway_events::THREAD_UPDATE},
+                { "THREAD_DELETE"                           , gateway_events::THREAD_DELETE},
+                { "THREAD_LIST_SYNC"                        , gateway_events::THREAD_LIST_SYNC},
+                { "THREAD_MEMBER_UPDATE"                    , gateway_events::THREAD_MEMBER_UPDATE},
+                { "THREAD_MEMBERS_UPDATE"                   , gateway_events::THREAD_MEMBERS_UPDATE},
+                { "ENTITLEMENT_CREATE"                      , gateway_events::ENTITLEMENT_CREATE},
+                { "ENTITLEMENT_UPDATE"                      , gateway_events::ENTITLEMENT_UPDATE},
+                { "ENTITLEMENT_DELETE"                      , gateway_events::ENTITLEMENT_DELETE},
+                { "GUILD_CREATE"                            , gateway_events::GUILD_CREATE},
+                { "GUILD_UPDATE"                            , gateway_events::GUILD_UPDATE},
+                { "GUILD_DELETE"                            , gateway_events::GUILD_DELETE},
+                { "GUILD_AUDIT_LOG_ENTRY_CREATE"            , gateway_events::GUILD_AUDIT_LOG_ENTRY_CREATE},
+                { "GUILD_BAN_ADD"                           , gateway_events::GUILD_BAN_ADD},
+                { "GUILD_BAN_REMOVE"                        , gateway_events::GUILD_BAN_REMOVE},
+                { "GUILD_EMOJIS_UPDATE"                     , gateway_events::GUILD_EMOJIS_UPDATE},
+                { "GUILD_STICKERS_UPDATE"                   , gateway_events::GUILD_STICKERS_UPDATE},
+                { "GUILD_INTEGRATIONS_UPDATE"               , gateway_events::GUILD_INTEGRATIONS_UPDATE},
+                { "GUILD_MEMBER_ADD"                        , gateway_events::GUILD_MEMBER_ADD},
+                { "GUILD_MEMBER_REMOVE"                     , gateway_events::GUILD_MEMBER_REMOVE},
+                { "GUILD_MEMBER_UPDATE"                     , gateway_events::GUILD_MEMBER_UPDATE},
+                { "GUILD_MEMBERS_CHUNK"                     , gateway_events::GUILD_MEMBERS_CHUNK},
+                { "GUILD_ROLE_CREATE"                       , gateway_events::GUILD_ROLE_CREATE},
+                { "GUILD_ROLE_UPDATE"                       , gateway_events::GUILD_ROLE_UPDATE},
+                { "GUILD_ROLE_DELETE"                       , gateway_events::GUILD_ROLE_DELETE},
+                { "GUILD_SCHEDULED_EVENT_CREATE"            , gateway_events::GUILD_SCHEDULED_EVENT_CREATE},
+                { "GUILD_SCHEDULED_EVENT_UPDATE"            , gateway_events::GUILD_SCHEDULED_EVENT_UPDATE},
+                { "GUILD_SCHEDULED_EVENT_DELETE"            , gateway_events::GUILD_SCHEDULED_EVENT_DELETE},
+                { "GUILD_SCHEDULED_EVENT_USER_ADD"          , gateway_events::GUILD_SCHEDULED_EVENT_USER_ADD},
+                { "GUILD_SCHEDULED_EVENT_USER_REMOVE"       , gateway_events::GUILD_SCHEDULED_EVENT_USER_REMOVE},
+                { "INTEGRATION_CREATE"                      , gateway_events::INTEGRATION_CREATE},
+                { "INTEGRATION_UPDATE"                      , gateway_events::INTEGRATION_UPDATE},
+                { "INTEGRATION_DELETE"                      , gateway_events::INTEGRATION_DELETE},
+                { "INTERACTION_CREATE"                      , gateway_events::INTERACTION_CREATE},
+                { "INVITE_CREATE"                           , gateway_events::INVITE_CREATE},
+                { "INVITE_DELETE"                           , gateway_events::INVITE_DELETE},
+                { "MESSAGE_CREATE"                          , gateway_events::MESSAGE_CREATE},
+                { "MESSAGE_UPDATE"                          , gateway_events::MESSAGE_UPDATE},
+                { "MESSAGE_DELETE"                          , gateway_events::MESSAGE_DELETE},
+                { "MESSAGE_DELETE_BULK"                     , gateway_events::MESSAGE_DELETE_BULK},
+                { "MESSAGE_REACTION_ADD"                    , gateway_events::MESSAGE_REACTION_ADD},
+                { "MESSAGE_REACTION_REMOVE"                 , gateway_events::MESSAGE_REACTION_REMOVE},
+                { "MESSAGE_REACTION_REMOVE_ALL"             , gateway_events::MESSAGE_REACTION_REMOVE_ALL},
+                { "MESSAGE_REACTION_REMOVE_EMOJI"           , gateway_events::MESSAGE_REACTION_REMOVE_EMOJI},
+                { "PRESENCE_UPDATE"                         , gateway_events::PRESENCE_UPDATE},
+                { "STAGE_INSTANCE_CREATE"                   , gateway_events::STAGE_INSTANCE_CREATE},
+                { "STAGE_INSTANCE_UPDATE"                   , gateway_events::STAGE_INSTANCE_UPDATE},
+                { "STAGE_INSTANCE_DELETE"                   , gateway_events::STAGE_INSTANCE_DELETE},
+                { "TYPING_START"                            , gateway_events::TYPING_START},
+                { "USER_UPDATE"                             , gateway_events::USER_UPDATE},
+                { "VOICE_STATE_UPDATE"                      , gateway_events::VOICE_STATE_UPDATE},
+                { "VOICE_SERVER_UPDATE"                     , gateway_events::VOICE_SERVER_UPDATE},
+                { "WEBHOOKS_UPDATE"                         , gateway_events::WEBHOOKS_UPDATE}
+            };
+
+            if (const auto& it = map.find(s); it != map.end()) return it->second;
+            return gateway_events::UNKNOWN;
+        }
+
+    }
+}
