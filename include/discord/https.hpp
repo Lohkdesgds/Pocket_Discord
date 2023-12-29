95a23adfdadce4cb05605715a7e610d6f99b263a
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2023-12-29 17:44:11 -0300

    - Cleaned up old format for more C++ classy format - Working on https client

---
+++ include/discord/https.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+
+
+namespace Lunaris {
+    namespace PocketDiscord {
+        
+        const char http_url[] = "https://discord.com/api/v10";
+        const char http_user_agent = ""
+        const int http_max_timeout = 2000;
+        
+        const char cert_http_path[] = "cert/api.pem";
+
+        //enum class http_request {
+        //    GET,
+        //    POST,
+        //    PUT,
+        //    PATCH,
+        //    DELETE,
+        //    HEAD,
+        //    NOTIFY,
+        //    SUBSCRIBE,
+        //    UNSUBSCRIBE,
+        //    OPTIONS
+        //};
+//
+        //class HTTPS {
+        //    HeapString m_token;             // from constructor
+        //public:
+        //    HTTPS(const char* token);
+        //};
+
+    }
+}
