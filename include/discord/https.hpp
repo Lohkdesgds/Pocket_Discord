b8f9e514117491c6f7f03b4dc2289189a98d8a9b
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2024-01-04 17:48:28 -0300

    Implemented single HTTPS per call implementation fully now - Works with chunked data - JSON parsed automatically - Rate limit implemented (not tested because of how ESP32 just can't do it that fast) - Implemented HTTPS_request movable now, avoiding pointer to single request in memory only

--- include/discord/https.hpp
+++ include/discord/https.hpp
@@ -7,6 +7,7 @@
 #include "esp_http_client.h"
 
 #include "defaults.h"
+#include "useful.h"
 #include "heapstring.h"
 #include "LJSON/json.h"
 
@@ -34,16 +35,44 @@ namespace Lunaris {
             OPTIONS
         };
 
-        class HTTPS_request{
-            esp_http_client_handle_t m_ref = nullptr;            
-            //std::unordered_map<HeapString, HeapString> m_headers;
-            char* m_data = nullptr; // expect Content-Length from headers
-            size_t m_data_write_off = 0;
+        class ChunkableChar : public IterateableJSONRef{
+            struct chunk {
+                char* data = nullptr;
+                size_t data_len = 0;
+                chunk* next = nullptr;
+                ~chunk();
+            };
 
+            chunk* m_c = nullptr;
+            chunk* m_last = nullptr;
+            size_t m_total = 0;
+        public:
+            ~ChunkableChar();
+            void append_chunk(const char*, size_t);
+
+            char get(const size_t at) const;
+            void read(char* ptr, const size_t len, const size_t at) const;
+            size_t max_off() const;
+        };
+
+        class HTTPS_request{
+
+            struct rate_limit {
+                uint64_t triggered_at = 0; // ms, use get_time_ms()
+                double wait_seconds = 0;
+                bool got_limited = false;
+            };
+
+            esp_http_client_handle_t m_ref = nullptr;
+            ChunkableChar* m_data = nullptr;
+            size_t m_data_write_off = 0;
             JSON* j = nullptr;
+            rate_limit m_curr_rl;
             int m_status = -1;
             bool m_was_prepared = false;
             bool m_finished = false;
+            
+            static rate_limit m_rl;
             
             void handle_https_events(esp_http_client_event_t*);
         public:
@@ -59,8 +88,6 @@ namespace Lunaris {
             const JSON* json() const;
             bool has_finished() const;
             int get_status() const;
-            const char* get_content_raw() const;
-            size_t get_content_raw_size() const;
         };
 
         class HTTPS {
@@ -68,13 +95,10 @@ namespace Lunaris {
             HeapString m_token;             // from constructor
             HeapString m_https_cert_perm;
             HeapString m_bot_header;
-            HTTPS_request* m_last = nullptr;
         public:
             HTTPS(const char* token);
-            ~HTTPS();
 
-            // only one open at a time.
-            HTTPS_request& post(const http_request, const char* path, const char* data = nullptr, const size_t data_len = 0);
+            HTTPS_request post(const http_request, const char* path, const char* data = nullptr, const size_t data_len = 0);
         };
 
     }
--- src/discord/https.cpp
+++ src/discord/https.cpp
@@ -8,7 +8,9 @@
 namespace Lunaris {
     namespace PocketDiscord {
 
-        static const char TAG[] = "HTTPS";        
+        static const char TAG[] = "HTTPS";
+        
+        HTTPS_request::rate_limit HTTPS_request::m_rl;
 
         esp_http_client_method_t http_request2esp(const http_request& me)
         {
@@ -38,6 +40,34 @@ namespace Lunaris {
             }
         }
 
+        const char* http_request2str(const http_request& me)
+        {
+            switch(me){                
+            case http_request::GET:
+                return "GET";
+            case http_request::POST:
+                return "POST";
+            case http_request::PUT:
+                return "PUT";
+            case http_request::PATCH:
+                return "PATCH";
+            case http_request::DELETE:
+                return "DELETE";
+            case http_request::HEAD:
+                return "HEAD";
+            case http_request::NOTIFY:
+                return "NOTIFY";
+            case http_request::SUBSCRIBE:
+                return "SUBSCRIBE";
+            case http_request::UNSUBSCRIBE:
+                return "UNSUBSCRIBE";
+            case http_request::OPTIONS:
+                return "OPTIONS";
+            default:
+                return "NULL"; // wtf
+            }
+        }
+
         // STRing EQual Case Insensitive
         bool streqci(const char* s1, const char* s2, const size_t l)
         {
@@ -48,68 +78,133 @@ namespace Lunaris {
             return true;
         }
         
+        ChunkableChar::chunk::~chunk()
+        {
+            DEL_EM(data);
+            DEL_IT(next);
+        }
+
+        ChunkableChar::~ChunkableChar()
+        {
+            DEL_IT(m_c);
+        }
+
+        void ChunkableChar::append_chunk(const char* s, size_t l)
+        {
+            if (!s || l == 0) return;
+
+            chunk* m_new = new chunk();
+            m_new->data = new char[l];
+            memcpy(m_new->data, s, l);
+            m_new->data_len = l;
+            m_total += l;
+
+            if (!m_c) {
+                m_c = m_new;
+                m_last = m_c;
+            }
+            else {
+                m_last->next = m_new;
+                m_last = m_last->next;
+            }
+        }
+
+        char ChunkableChar::get(const size_t at) const
+        {
+            if (at >= m_total) return '\0';
+            size_t p = at;
+            for(chunk* c = m_c; c != nullptr; c = c->next)
+            {
+                if (p < c->data_len) return c->data[p];
+                p -= c->data_len; // cannot result in less than 0 because of previous test
+            }
+            return '\0'; // somehow?
+        }
+
+        void ChunkableChar::read(char* ptr, const size_t len, const size_t at) const
+        {
+            memset(ptr, '\0', len);
+
+            if (at >= m_total) return;
+            
+            size_t p = at; // offsetting on the way
+            size_t l = len; // adjust on the way
+            for(chunk* c = m_c; c != nullptr; c = c->next)
+            {
+                if (p < c->data_len) { // can read
+                    const size_t max_read = c->data_len - p;
+                    const size_t act_read = max_read < l ? max_read : l;
+                    l -= act_read;
+                    memcpy(ptr, c->data + p, act_read);
+                    ptr += act_read; // offset pointer itself for easier run
+                }
+                else {
+                    if (p >= c->data_len) p -= c->data_len;
+                    else p = 0; // I know this shouldn't be necessary, but what if
+                }
+            }
+        }
+
+        size_t ChunkableChar::max_off() const
+        {
+            return m_total;
+        }
+
+        
         void HTTPS_request::handle_https_events(esp_http_client_event_t* e)
         {
             switch(e->event_id) {
             case HTTP_EVENT_ERROR:
-                ESP_LOGE(TAG, "HTTP_EVENT_ERROR!");
+                //ESP_LOGE(TAG, "HTTP_EVENT_ERROR!");
                 break;
             case HTTP_EVENT_ON_CONNECTED:
-                ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED!");
+                //ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED!");
+                m_curr_rl.triggered_at = 0;
+                m_curr_rl.wait_seconds = 0;
+                m_curr_rl.got_limited = false;
                 break;
             case HTTP_EVENT_HEADERS_SENT: // all headers were sent
-                ESP_LOGI(TAG, "HTTP_EVENT_HEADERS_SENT!");
+                //ESP_LOGI(TAG, "HTTP_EVENT_HEADERS_SENT!");
                 break;
             case HTTP_EVENT_ON_HEADER:
-                ESP_LOGW(TAG, "New header: %s -> %s", e->header_key, e->header_value);
-                if (streqci(e->header_key, "Content-Length", 14)) {
-                    const auto ll = atoll(e->header_value);
-                    ESP_LOGI(TAG, "Preparing for content of size %lli", ll);
-                    m_was_prepared = true;
-                    m_data_write_off = 0;
-                    //m_data.prepare(ll + 1);
-                    if (m_data) delete[] m_data; // Just to be sure u know
-                    m_data = new char[ll + 1];
-                    memset(m_data, '\0', ll + 1);
+                //ESP_LOGW(TAG, "New header: %s -> %s", e->header_key, e->header_value);
+                if (streqci(e->header_key, "X-RateLimit-Remaining", 21)) {
+                    const int i = atoi(e->header_value);
+                    m_curr_rl.got_limited |= (i <= 1);
                 }
+                else if (streqci(e->header_key, "X-RateLimit-Reset-After", 23)) {
+                    m_curr_rl.wait_seconds = strtod(e->header_value, nullptr);
+                    m_curr_rl.triggered_at = get_time_ms();
+                }         
                 break;
             case HTTP_EVENT_ON_DATA:
-                ESP_LOGW(TAG, "New data: %.*s [%i]", e->data_len, (char*)e->data, e->data_len);
-                if (m_was_prepared) {
-                    memcpy(m_data + m_data_write_off, (const char*)e->data, e->data_len);
-                    m_data_write_off += e->data_len;
-                }
-                else {                    
-                    ESP_LOGE(TAG, "DYNAMIC DATA BEING USED BECAUSE CONTENT-LENGTH HEADER WAS MISSING.");
-
-                    char* old = m_data;
-                    m_data = new char[m_data_write_off + e->data_len + 1];
-                    if (old) {
-                        memcpy(m_data, old, m_data_write_off);
-                        delete[] old;
-                    }
-                    memcpy(m_data + m_data_write_off, e->data, e->data_len);
-                    m_data_write_off += e->data_len;
-                    m_data[m_data_write_off] = '\0';                    
-
-                    //m_data.append((const char*)e->data, e->data_len);
-                    //m_data_write_off += e->data_len;
-                }
+                //ESP_LOGW(TAG, "New data: %.*s [%i]", e->data_len, (char*)e->data, e->data_len);
+                if (!m_data) m_data = new ChunkableChar();
+                m_data->append_chunk((const char*)e->data, e->data_len);
                 break;
             case HTTP_EVENT_ON_FINISH:
-                ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH!");
+                //ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH!");
                 {
+                    if (m_curr_rl.got_limited) {
+                        const double needed_delay_before = m_rl.got_limited ? (m_rl.wait_seconds - 0.001 * (get_time_ms() - m_rl.triggered_at)) : 0.0;
+                        const double needed_delay_now    = m_curr_rl.wait_seconds - 0.001 * (get_time_ms() - m_curr_rl.triggered_at);
+                        m_rl.triggered_at = get_time_ms();
+                        m_rl.wait_seconds = needed_delay_now > needed_delay_before ? needed_delay_now : needed_delay_before;
+                        m_rl.got_limited = true;
+                    }
+
                     if (m_data) {
-                        j = new JSON(m_data, m_data_write_off);
+                        j = new JSON((ChunkableChar*&&)m_data);
+                        m_data = nullptr;
                     }
                     m_finished = true;
                 }
                 break;
             case HTTP_EVENT_DISCONNECTED:
-                ESP_LOGE(TAG, "HTTP_EVENT_DISCONNECTED!");
+                //ESP_LOGE(TAG, "HTTP_EVENT_DISCONNECTED!");
                 break;
             case HTTP_EVENT_REDIRECT:
-                ESP_LOGE(TAG, "HTTP_EVENT_REDIRECT!");
+                //ESP_LOGE(TAG, "HTTP_EVENT_REDIRECT!");
                 break;
             default:
                 break;
@@ -118,6 +213,14 @@ namespace Lunaris {
 
         HTTPS_request::HTTPS_request(const http_request r, const char* path, const char* data, const size_t data_len, const HeapString& m_https_cert_perm, const HeapString& m_bot_header)
         {
+            if (m_rl.got_limited) {
+                m_rl.got_limited = false;
+                const double secs = (m_rl.wait_seconds - 0.001 * (get_time_ms() - m_rl.triggered_at));
+                const uint64_t msecs = static_cast<uint64_t>(1000.0 * secs + 1);
+                ESP_LOGW(TAG, "Got rate-limited, waiting %llu ms", msecs);
+                vTaskDelay(pdMS_TO_TICKS(msecs));
+            }
+
             esp_http_client_config_t config = {
                 .url = http_url,
                 .cert_pem = m_https_cert_perm.c_str(),
@@ -146,11 +249,6 @@ namespace Lunaris {
             esp_http_client_set_header(m_ref, "Authorization", m_bot_header.c_str());
             esp_http_client_set_header(m_ref, "Content-Type", "application/json");
 
-            ESP_LOGI(TAG, "Headers set:");
-            ESP_LOGI(TAG, "- User-Agent -> %s", http_user_agent);
-            ESP_LOGI(TAG, "- Authorization -> %s", m_bot_header.c_str());
-            ESP_LOGI(TAG, "- Content-Type -> %s", "application/json");
-
             if (data && data_len > 0) esp_http_client_set_post_field(m_ref, data, data_len);
 
             esp_err_t err = ESP_OK;
@@ -169,6 +267,19 @@ namespace Lunaris {
             }
 
             m_status = esp_http_client_get_status_code(m_ref);
+            ESP_LOGI(TAG, "Performed HTTP %s @ %s; Got status=%i.", http_request2str(r), path, m_status);
+        }
+
+        HTTPS_request::HTTPS_request(HTTPS_request&& o) noexcept
+        {
+            EXCHANGE(m_ref, o.m_ref, nullptr);
+            EXCHANGE(m_data, o.m_data, nullptr);
+            EXCHANGE(m_data_write_off, o.m_data_write_off, 0);
+            EXCHANGE(j, o.j, nullptr);
+            EXCHANGE(m_curr_rl, o.m_curr_rl, {});
+            EXCHANGE(m_status, o.m_status, -1);
+            EXCHANGE(m_was_prepared, o.m_was_prepared, false);
+            EXCHANGE(m_finished, o.m_finished, false);
         }
 
         HTTPS_request::~HTTPS_request()
@@ -192,16 +303,6 @@ namespace Lunaris {
         {
             return m_status;
         }
-
-        const char* HTTPS_request::get_content_raw() const
-        {
-            return m_data;
-        }
-        
-        size_t HTTPS_request::get_content_raw_size() const
-        {
-            return m_data_write_off;
-        }
         
         HTTPS::HTTPS(const char* token)
             : m_token(token)
@@ -219,20 +320,12 @@ namespace Lunaris {
             m_bot_header.append(m_token.c_str(), m_token.size());
             
             ESP_LOGI(TAG, "HTTPS ready.");
-
-            //start_https();
-        }
-
-        HTTPS::~HTTPS()
-        {            
-            DEL_IT(m_last);
         }
         
         // following https://github.com/espressif/esp-idf/blob/ece73357caa6c766770136b82639964870e340ba/examples/protocols/esp_http_client/main/esp_http_client_example.c#L623 this time
-        HTTPS_request& HTTPS::post(const http_request r, const char* path, const char* data, const size_t data_len)
+        HTTPS_request HTTPS::post(const http_request r, const char* path, const char* data, const size_t data_len)
         {
-            if (m_last) delete m_last;
-            return *(m_last = new HTTPS_request(r, path, data, data_len, m_https_cert_perm, m_bot_header));
+            return HTTPS_request(r, path, data, data_len, m_https_cert_perm, m_bot_header);
         }
         
 
